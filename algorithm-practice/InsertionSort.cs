4990d1265d8c57eeea9ea3771ad8b35efdbb0510
Bret Gourdie <bretgourdie@gmail.com>
2017-02-21 13:20:07 -0500

    Implemented InsertionSort's Sort (untested)

--- algorithm-practice/InsertionSort.cs
+++ algorithm-practice/InsertionSort.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace algorithm_practice
 {
-    public class InsertionSort<T> : ISortable<T>
+    public class InsertionSort<T> : ISortable<T> where T : IComparable
     {
+        public InsertionSort()
+        {
+            
+        }
+
+        public T[] Sort(T[] array)
+        {
+            for (int curIndex = 0; curIndex < array.Length; curIndex++)
+            {
+                var curVal = array[curIndex];
+
+                bool inserted = false;
+                for (int prevIndex = curIndex - 1; prevIndex >= 0 && !inserted; prevIndex--)
+                {
+                    var prevVal = array[prevIndex];
+
+                    if (prevVal.CompareTo(curVal) > 0)
+                    {
+                        array[prevIndex + 1] = prevVal;
+                    }
+
+                    else if (prevVal.CompareTo(curVal) < 0)
+                    {
+                        array[prevIndex + 1] = curVal;
+                        inserted = true;
+                    }
+                }
+            }
+
+            return array;
+        }
     }
 }
