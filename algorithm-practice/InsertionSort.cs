8a70b23a395ee03fc6c73b1894a3f39e35461e7a
Bret Gourdie <bretgourdie@gmail.com>
2017-02-21 10:41:41 -0500

    Initial add of all files

---
+++ algorithm-practice/InsertionSort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_practice
+{
+    public class InsertionSort<T> : ISortable<T>
+    {
+    }
+}
