fa08bd9f589f9d68a57f897985a5a6d00fa2d03a
Bret Gourdie <bretgourdie@gmail.com>
2017-02-21 13:35:20 -0500

    Implemented test for Insertion Sort

---
+++ algorithm-practice-tests/InsertionSortTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algorithm_practice.Tests
+{
+    [TestClass]
+    public class InsertionSortTests
+    {
+        [TestMethod]
+        public void BasicIntsTest()
+        {
+            var inssort = new InsertionSort();
+
+            var input = new int[] { 6, 5, 3, 1, 8, 7, 2, 4 };
+            var expected = new int[input.Length];
+            Array.Copy(input, expected, input.Length);
+            Array.Sort(expected);
+
+            var result = inssort.Sort<int>(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
