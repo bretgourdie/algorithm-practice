e2b887cdb99c778ad1d754acd851dead987c830e
Bret Gourdie <bretgourdie@gmail.com>
2017-02-22 13:09:49 -0500

    Added common Node class

---
+++ algorithm-practice/Common/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_practice.Common
+{
+    public class Node<T>
+    {
+        public T Data { get; protected set; }
+        Node<T> next { get; set; }
+
+        public Node(T data)
+        {
+            this.Data = data;
+        }
+    }
+}
