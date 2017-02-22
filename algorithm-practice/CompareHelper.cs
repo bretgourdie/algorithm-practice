c6babb775f50e910571a14c3f138d239109b5c5f
Bret Gourdie <bretgourdie@gmail.com>
2017-02-22 09:57:27 -0500

    Added CompareHelper
    This will reduce the number of hard-coded numbers and help me think about what the CompareTo result means

---
+++ algorithm-practice/CompareHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_practice
+{
+    internal class CompareHelper
+    {
+        public enum Comparison
+        {
+            GreaterThan,
+            LessThan,
+            EqualTo
+        }
+
+        public static Comparison ComparesTo<T>(T a, T b) where T : IComparable
+        {
+            var comparisonResult = a.CompareTo(b);
+
+            if (comparisonResult < 0)
+            {
+                return Comparison.LessThan;
+            }
+
+            else if (comparisonResult == 0)
+            {
+                return Comparison.EqualTo;
+            }
+
+            else if (comparisonResult > 0)
+            {
+                return Comparison.GreaterThan;
+            }
+
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
