f9663a3a372de38931603a34211e0de779d024ba
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2017-02-21 23:04:35 -0500

    Add selection sort test file

---
+++ algorithm-practice-tests/SelectionSortTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithm_practice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using algorithm_practice;
+
+namespace algorithm_practice.Tests
+{
+    [TestClass()]
+    public class SelectionSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            var selsort = new SelectionSort();
+            var input = new int[] { 5, 2, 7, 1, 4, 3 };
+            var expected = new int[input.Length];
+            Array.Copy(input, expected, input.Length);
+            Array.Sort(expected);
+
+            var result = selsort.Sort<int>(input);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
