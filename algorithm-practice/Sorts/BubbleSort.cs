ee17330fe75297b596908a7e5eaa2caf8bafd9a7
Bret Gourdie <bretgourdie@gmail.com>
2017-02-22 10:05:12 -0500

    Implemented BubbleSort (untested)

--- algorithm-practice/Sorts/BubbleSort.cs
+++ algorithm-practice/Sorts/BubbleSort.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace algorithm_practice.Sorts
 {
-    class BubbleSort
+    public class BubbleSort : ISortable
     {
+
+        public T[] Sort<T>(T[] array)
+        {
+            for (int ii = 0; ii < array.Length; ii++)
+            {
+                for (int jj = 0; jj < array.Length - ii - 1; jj++)
+                {
+                    var result = CompareHelper.ComparesTo<T>(
+                        array[jj],
+                        array[jj + 1]);
+
+                    if (result == Comparison.GreaterThan)
+                    {
+                        var temp = array[jj];
+                        array[jj] = array[jj + 1];
+                        array[jj + 1] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
     }
 }
