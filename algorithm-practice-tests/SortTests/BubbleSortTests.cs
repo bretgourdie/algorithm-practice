d56324f17ef8e17fe9c97cd98c05dc090c78e3e8
Bret Gourdie <bretgourdie@gmail.com>
2017-02-22 10:16:21 -0500

    Added "SameInt" test

--- algorithm-practice-tests/SortTests/BubbleSortTests.cs
+++ algorithm-practice-tests/SortTests/BubbleSortTests.cs
@@ -19,5 +19,19 @@ namespace algorithm_practice.Sorts.SortTests
 
             CollectionAssert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void BubbleSort_SameIntsTest()
+        {
+            var bsort = new BubbleSort();
+            var input = new int[] { 7, 2, 4, 3, 1, 4, 5, 2, 4 };
+            var expected = new int[input.Length];
+            Array.Copy(input, expected, input.Length);
+            Array.Sort(expected);
+
+            var result = bsort.Sort(input);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
     }
 }
