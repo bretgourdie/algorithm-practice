c5f3d390c2c5495b8bcffc64e67042f1348d31b5
Bret Gourdie <bretgourdie@gmail.com>
2017-02-23 16:40:44 -0500

    Added NodeList class (belongs to Node)

---
+++ algorithm-practice/Common/NodeList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_practice.Common
+{
+    public class NodeList<T> : 
+        System.Collections.ObjectModel.Collection<Node<T>>
+    {
+        public NodeList() : base() { }
+
+        public NodeList(int initialSize)
+        {
+            for (int ii = 0; ii < initialSize; ii++)
+            {
+                base.Items.Add(default(Node<T>));
+            }
+        }
+
+        public Node<T> FindByValue(T value)
+        {
+            foreach (var node in Items)
+            {
+                if (node.Value.Equals(value))
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+    }
+}
