a25c8300c55c61f5d7bb93df94e499e55267808d
Bret Gourdie <bretgourdie@gmail.com>
2017-02-24 16:58:49 -0500

    Added GraphNode skeleton

---
+++ algorithm-practice/Common/GraphNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_practice.Common
+{
+    public class GraphNode<T> : Node<T>
+    {
+
+    }
+}
